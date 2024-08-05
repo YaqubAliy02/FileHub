b636406cbeabe0530a91ced666893f07ab48e232
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-05 14:23:31 +0500

    Add project files.

---
+++ FileHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
